0d3f48e866730dd153a94aefbbe4783c151ddabd
Gang2543YT <152345933+Gang2543YT@users.noreply.github.com>
2026-08-03 02:42:08 +0000

    modified:   main.py 	modified:   templates/index.html

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import eventlet
 eventlet.monkey_patch()
 
 import os
+import math
+import random
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room, leave_room, emit
 
@@ -9,7 +11,27 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'terra-quest-secret-key'
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
+WORLD_WIDTH = 120
+WORLD_HEIGHT = 45
+
 rooms_players = {}
+rooms_worlds = {}
+
+def generate_server_world():
+    world = [[0 for _ in range(WORLD_WIDTH)] for _ in range(WORLD_HEIGHT)]
+    surface_height = 18
+    for x in range(WORLD_WIDTH):
+        surface_height += math.floor(math.sin(x * 0.15) * 1.5 + (random.random() * 0.6 - 0.3))
+        surface_height = max(12, min(28, surface_height))
+
+        for y in range(surface_height, WORLD_HEIGHT):
+            if y == surface_height:
+                world[y][x] = 3  # Grass
+            elif y < surface_height + 5:
+                world[y][x] = 1  # Dirt
+            else:
+                world[y][x] = 2  # Stone
+    return world
 
 @app.route('/')
 def home():
@@ -21,6 +43,9 @@ def handle_join(data):
     name = data.get('name', 'Hero')
     
     join_room(room)
+    
+    if room not in rooms_worlds:
+        rooms_worlds[room] = generate_server_world()
     
     player_data = {
         'id': request.sid,
@@ -36,8 +61,9 @@ def handle_join(data):
         rooms_players[room] = {}
     rooms_players[room][request.sid] = player_data
     
+    # Send the current world map and players to the newly connected user
+    emit('init_world', {'world': rooms_worlds[room], 'players': rooms_players[room]})
     emit('player_joined', player_data, to=room, include_self=False)
-    emit('current_players', rooms_players[room])
 
 @socketio.on('player_update')
 def handle_update(data):
@@ -54,9 +80,14 @@ def handle_update(data):
 @socketio.on('update_tile')
 def handle_tile_update(data):
     room = data.get('room')
-    if room:
-        # Broadcast block break/placement changes to everyone else in the room
-        emit('tile_updated', data, to=room, include_self=False)
+    x = data.get('x')
+    y = data.get('y')
+    tile = data.get('tile')
+    
+    if room and room in rooms_worlds:
+        if 0 <= x < WORLD_WIDTH and 0 <= y < WORLD_HEIGHT:
+            rooms_worlds[room][y][x] = tile
+            emit('tile_updated', data, to=room, include_self=False)
 
 @socketio.on('disconnect')
 def handle_disconnect():
@@ -65,7 +96,10 @@ def handle_disconnect():
             del players[request.sid]
             emit('player_left', {'id': request.sid}, to=room)
             if not players:
-                del rooms_players[room]
+                if room in rooms_players:
+                    del rooms_players[room]
+                if room in rooms_worlds:
+                    del rooms_worlds[room]
             break
 
 if __name__ == '__main__':
